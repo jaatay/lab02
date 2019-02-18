8e5f149afcc3f172424360e83b4cb4f2ad0c31b9
Jesse Atay <jesse.atay@gmail.com>
2019-02-17 18:52:40 -0800

    readme updates

--- objects/flyingStuff/flyingVehicle.js
+++ objects/flyingStuff/flyingVehicle.js
@@ -1,35 +1,35 @@
 'use strict';
 
 //Flying vehicle parent
-const FlyingVehicle = function(name, wings) {
+function FlyingVehicle(name, wings) {
   this.name = name;
   this.wings = wings;
 };
 
-Vehicle.prototype.fly = () => {
+FlyingVehicle.prototype.fly = function(){
   return 'Moving Up';
 };
 
-Vehicle.prototype.crash = () => {
+FlyingVehicle.prototype.crash = function() {
   return 'Whoops!';
 };
 
 //Airplane vehicle child
-const AirPlane = function(name) {
+function AirPlane(name) {
   FlyingVehicle.call(this, name, 2);
 };
 
 AirPlane.prototype = new FlyingVehicle();
 
 //Helicopter vehicle child
-const Helicopter = function(name) {
+function Helicopter(name) {
   FlyingVehicle.call(this,name,0);
 };
 
 Helicopter.prototype = new FlyingVehicle();
 
-Helicopter.prototype.roll = () => {
+Helicopter.prototype.roll = function roll() {
   return 'Do a barrel roll!';
 };
 
-module.exports = {Airplane, Helicopter};
+module.exports = {FlyingVehicle, AirPlane, Helicopter};
